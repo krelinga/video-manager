de0fd62f6e3bdce0433d1e41de3927852fe6f9eb
Andy Kreling <krelinga@gmail.com>
2025-11-29 21:49:04 +0000

    don't issue a continuation token if there are no more records.

--- internal/services/catalog/list_movie_edition_kind.go
+++ internal/services/catalog/list_movie_edition_kind.go
@@ -21,13 +21,18 @@ func (s *CatalogServiceHandler) ListMovieEditionKind(ctx context.Context, req *c
 		Max:  100,
 	}
 	const query = "SELECT id, name, is_default FROM catalog_movie_edition_kinds WHERE id > $1 ORDER BY id ASC LIMIT $2"
-	rows, _ := s.DBPool.Query(ctx, query, lastSeenId, sizer.Size())
+	rows, _ := s.DBPool.Query(ctx, query, lastSeenId, sizer.Size() + 1)
 	defer rows.Close()
 	var id uint32
 	var name string
 	var isDefault bool
+	var more bool
 	response := connect.NewResponse(&catalogv1.ListMovieEditionKindResponse{})
 	_, err = pgx.ForEachRow(rows, []any{&id, &name, &isDefault}, func() error {
+		if len(response.Msg.MovieEditionKinds) == int(sizer.Size()) {
+			more = true
+			return nil
+		}
 		response.Msg.MovieEditionKinds = append(response.Msg.MovieEditionKinds, &catalogv1.MovieEditionKind{
 			Id:        id,
 			Name:      name,
@@ -39,6 +44,8 @@ func (s *CatalogServiceHandler) ListMovieEditionKind(ctx context.Context, req *c
 		err = fmt.Errorf("failed to query movie edition kinds: %w", err)
 		return nil, connect.NewError(connect.CodeInternal, err)
 	}
-	response.Msg.NextPageToken = page.FromLastSeenId(id)
+	if more {
+		response.Msg.NextPageToken = page.FromLastSeenId(id)
+	}
 	return response, nil
 }
