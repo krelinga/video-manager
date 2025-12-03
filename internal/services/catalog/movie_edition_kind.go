cf402b142495bd91351db075aa445ab91f1d145a
Andy Kreling <krelinga@gmail.com>
2025-12-03 19:10:53 +0000

    Move MovieEditionKinds into their own file.
    also update API dependency.
    
    also implement ListMovieEditionKinds().

---
+++ internal/services/catalog/movie_edition_kind.go
@@ -0,0 +1,57 @@
+package catalog
+
+import (
+	"context"
+
+	"github.com/krelinga/video-manager-api/go/vmapi"
+	"github.com/krelinga/video-manager/internal/lib/vmpage"
+)
+
+func (s *CatalogService) ListMovieEditionKinds(ctx context.Context, request vmapi.ListMovieEditionKindsRequestObject) (vmapi.ListMovieEditionKindsResponseObject, error) {
+	const sql = "SELECT id, name, is_default FROM catalog_movie_edition_kinds WHERE id > @lastSeenId ORDER BY id ASC LIMIT @limit;"
+	var entries []vmapi.MovieEditionKind
+	query := &vmpage.ListQuery{
+		Sql:       sql,
+		Want:      request.Params.PageSize,
+		Default:   50,
+		Max:       100,
+		PageToken: request.Params.PageToken,
+	}
+	type row struct {
+		Id        uint32
+		Name      string
+		IsDefault bool
+	}
+	nextPageToken, err := vmpage.ListPtr(ctx, s.Db, query, func(r *row) uint32 {
+		entries = append(entries, vmapi.MovieEditionKind{
+			Id:        r.Id,
+			Name:      r.Name,
+			IsDefault: r.IsDefault,
+		})
+		return r.Id
+	})
+	if err != nil {
+		return nil, err
+	}
+	resp := vmapi.ListMovieEditionKinds200JSONResponse{
+		MovieEditionKinds: entries,
+		NextPageToken:     nextPageToken,
+	}
+	return resp, nil
+}
+
+func (s *CatalogService) PostMovieEditionKind(ctx context.Context, request vmapi.PostMovieEditionKindRequestObject) (vmapi.PostMovieEditionKindResponseObject, error) {
+	return nil, nil // TODO
+}
+
+func (s *CatalogService) DeleteMovieEditionKind(ctx context.Context, request vmapi.DeleteMovieEditionKindRequestObject) (vmapi.DeleteMovieEditionKindResponseObject, error) {
+	return nil, nil // TODO
+}
+
+func (s *CatalogService) GetMovieEditionKind(ctx context.Context, request vmapi.GetMovieEditionKindRequestObject) (vmapi.GetMovieEditionKindResponseObject, error) {
+	return nil, nil // TODO
+}
+
+func (s *CatalogService) PatchMovieEditionKind(ctx context.Context, request vmapi.PatchMovieEditionKindRequestObject) (vmapi.PatchMovieEditionKindResponseObject, error) {
+	return nil, nil // TODO
+}
--- internal/services/catalog/rpc.go
+++ internal/services/catalog/rpc.go
@@ -7,7 +7,7 @@ import (
 	"github.com/krelinga/video-manager/internal/lib/vmdb"
 )
 
-type CatalogService struct{
+type CatalogService struct {
 	Db vmdb.DbRunner
 }
 
@@ -15,9 +15,11 @@ type CatalogService struct{
 func (s *CatalogService) ListCards(ctx context.Context, request vmapi.ListCardsRequestObject) (vmapi.ListCardsResponseObject, error) {
 	name := "Example Card"
 	return vmapi.ListCards200JSONResponse{
-		{
-			Id:   1,
-			Name: &name,
+		Cards: []vmapi.Card{
+			{
+				Id:   1,
+				Name: &name,
+			},
 		},
 	}, nil
 }
@@ -35,26 +37,5 @@ func (s *CatalogService) GetCard(ctx context.Context, request vmapi.GetCardReque
 }
 
 func (s *CatalogService) PatchCard(ctx context.Context, request vmapi.PatchCardRequestObject) (vmapi.PatchCardResponseObject, error) {
-	return nil, nil // TODO
-}
-
-// Movie Edition Kind endpoints
-func (s *CatalogService) ListMovieEditionKinds(ctx context.Context, request vmapi.ListMovieEditionKindsRequestObject) (vmapi.ListMovieEditionKindsResponseObject, error) {
-	return nil, nil // TODO
-}
-
-func (s *CatalogService) PostMovieEditionKind(ctx context.Context, request vmapi.PostMovieEditionKindRequestObject) (vmapi.PostMovieEditionKindResponseObject, error) {
-	return nil, nil // TODO
-}
-
-func (s *CatalogService) DeleteMovieEditionKind(ctx context.Context, request vmapi.DeleteMovieEditionKindRequestObject) (vmapi.DeleteMovieEditionKindResponseObject, error) {
-	return nil, nil // TODO
-}
-
-func (s *CatalogService) GetMovieEditionKind(ctx context.Context, request vmapi.GetMovieEditionKindRequestObject) (vmapi.GetMovieEditionKindResponseObject, error) {
-	return nil, nil // TODO
-}
-
-func (s *CatalogService) PatchMovieEditionKind(ctx context.Context, request vmapi.PatchMovieEditionKindRequestObject) (vmapi.PatchMovieEditionKindResponseObject, error) {
 	return nil, nil // TODO
 }
