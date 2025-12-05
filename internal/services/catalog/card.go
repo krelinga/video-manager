ccb1172ec59af2e6aed2fe6ecf194b6b047865cc
Andy Kreling <krelinga@gmail.com>
2025-12-05 23:12:43 +0000

    move method stubs for cards to their own file.

---
+++ internal/services/catalog/card.go
@@ -0,0 +1,28 @@
+package catalog
+
+import (
+	"context"
+
+	"github.com/krelinga/video-manager-api/go/vmapi"
+)
+
+// Card endpoints
+func (s *CatalogService) ListCards(ctx context.Context, request vmapi.ListCardsRequestObject) (vmapi.ListCardsResponseObject, error) {
+	return nil, nil // TODO
+}
+
+func (s *CatalogService) PostCard(ctx context.Context, request vmapi.PostCardRequestObject) (vmapi.PostCardResponseObject, error) {
+	return nil, nil // TODO
+}
+
+func (s *CatalogService) DeleteCard(ctx context.Context, request vmapi.DeleteCardRequestObject) (vmapi.DeleteCardResponseObject, error) {
+	return nil, nil // TODO
+}
+
+func (s *CatalogService) GetCard(ctx context.Context, request vmapi.GetCardRequestObject) (vmapi.GetCardResponseObject, error) {
+	return nil, nil // TODO
+}
+
+func (s *CatalogService) PatchCard(ctx context.Context, request vmapi.PatchCardRequestObject) (vmapi.PatchCardResponseObject, error) {
+	return nil, nil // TODO
+}
--- internal/services/catalog/rpc.go
+++ internal/services/catalog/rpc.go
@@ -1,41 +1,9 @@
 package catalog
 
 import (
-	"context"
-
-	"github.com/krelinga/video-manager-api/go/vmapi"
 	"github.com/krelinga/video-manager/internal/lib/vmdb"
 )
 
 type CatalogService struct {
 	Db vmdb.DbRunner
-}
-
-// Card endpoints
-func (s *CatalogService) ListCards(ctx context.Context, request vmapi.ListCardsRequestObject) (vmapi.ListCardsResponseObject, error) {
-	name := "Example Card"
-	return vmapi.ListCards200JSONResponse{
-		Cards: []vmapi.Card{
-			{
-				Id:   1,
-				Name: name,
-			},
-		},
-	}, nil
-}
-
-func (s *CatalogService) PostCard(ctx context.Context, request vmapi.PostCardRequestObject) (vmapi.PostCardResponseObject, error) {
-	return nil, nil // TODO
-}
-
-func (s *CatalogService) DeleteCard(ctx context.Context, request vmapi.DeleteCardRequestObject) (vmapi.DeleteCardResponseObject, error) {
-	return nil, nil // TODO
-}
-
-func (s *CatalogService) GetCard(ctx context.Context, request vmapi.GetCardRequestObject) (vmapi.GetCardResponseObject, error) {
-	return nil, nil // TODO
-}
-
-func (s *CatalogService) PatchCard(ctx context.Context, request vmapi.PatchCardRequestObject) (vmapi.PatchCardResponseObject, error) {
-	return nil, nil // TODO
 }
