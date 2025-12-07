3c1f573bfe642f4e295a7136ce4fdecc992357a7
Andy Kreling <krelinga@gmail.com>
2025-12-07 06:31:07 +0000

    Split out setup() from Up(), and implemente Down().

--- internal/lib/migrate/migrate.go
+++ internal/lib/migrate/migrate.go
@@ -27,23 +27,45 @@ func (l logger) Verbose() bool {
 	return false
 }
 
-func Up(cfg *config.Postgres) error {
+func setup(cfg *config.Postgres) (*migrate.Migrate, error) {
 	d, err := iofs.New(migrationsFS, "migrations")
 	if err != nil {
-		return fmt.Errorf("%w: failed to create iofs source: %w", Err, err)
+		return nil, fmt.Errorf("%w: failed to create iofs source: %w", Err, err)
 	}
 	m, err := migrate.NewWithSourceInstance("embed", d, cfg.URL())
 	if err != nil {
-		return fmt.Errorf("%w: failed to create migrate instance: %w", Err, err)
+		return nil, fmt.Errorf("%w: failed to create migrate instance: %w", Err, err)
 	}
 	m.Log = logger{Logger: log.Default()}
+	return m, nil
+}
 
-	log.Println("Starting database migrations...")
+func Up(cfg *config.Postgres) error {
+	m, err := setup(cfg)
+	if err != nil {
+		return err
+	}
+	log.Println("Starting database UP migrations...")
 	err = m.Up()
 	if err != nil && err != migrate.ErrNoChange {
 		return fmt.Errorf("%w: migration failed: %w", Err, err)
 	}
 
-	log.Println("Database migrations completed successfully.")
+	log.Println("Database UP migrations completed successfully.")
+	return nil
+}
+
+func Down(cfg *config.Postgres) error {
+	m, err := setup(cfg)
+	if err != nil {
+		return err
+	}
+	log.Println("Starting database DOWN migrations...")
+	err = m.Down()
+	if err != nil && err != migrate.ErrNoChange {
+		return fmt.Errorf("%w: migration failed: %w", Err, err)
+	}
+
+	log.Println("Database DOWN migrations completed successfully.")
 	return nil
 }
