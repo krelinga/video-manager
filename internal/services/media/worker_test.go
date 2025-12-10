572752a00eee3dcd2880752a1e6b40aeae229857
Andy Kreling <krelinga@gmail.com>
2025-12-10 04:28:39 +0000

    refactor worker_test to be table-driven.

--- internal/services/media/worker_test.go
+++ internal/services/media/worker_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/krelinga/go-libs/deep"
 	"github.com/krelinga/go-libs/exam"
+	"github.com/krelinga/go-libs/match"
 	"github.com/krelinga/video-manager/internal/lib/vmtest"
 	"github.com/krelinga/video-manager/internal/services/media"
 )
@@ -19,13 +20,36 @@ func TestDvdIngestionWorker(t *testing.T) {
 	db := pg.DbRunner(e)
 	// mediaService := NewMediaService(e, pg)
 
-	e.Run("No work to do", func(e exam.E) {
-		defer pg.Reset(e)
-		worker := &media.DvdIngestionWorker{
-			Db:           db,
-		}
-		didWork, err := worker.Scan(ctx)
-		exam.Nil(e, env, err).Log(deep.Format(env, err)).Must()
-		exam.Equal(e, env, didWork, false).Log(didWork)
-	})
+	tests := []struct {
+		loc         exam.Loc
+		name        string
+		setup       func(e exam.E)
+		wantDidWork match.Matcher
+		wantErr     match.Matcher
+		check       func(e exam.E)
+	}{
+		{
+			loc:         exam.Here(),
+			name:        "No work to do",
+			wantDidWork: match.Equal(false),
+			wantErr:     match.Nil(),
+		},
+	}
+	for _, tt := range tests {
+		e.Run(tt.name, func(e exam.E) {
+			defer pg.Reset(e)
+			worker := &media.DvdIngestionWorker{
+				Db: db,
+			}
+			if tt.setup != nil {
+				tt.setup(e)
+			}
+			didWork, err := worker.Scan(ctx)
+			exam.Match(e, env, err, tt.wantErr).Log(err).Must()
+			exam.Match(e, env, didWork, tt.wantDidWork).Log(didWork)
+			if tt.check != nil {
+				tt.check(e)
+			}
+		})
+	}
 }
