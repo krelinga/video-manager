e62763e69f4ddc2e66f137b9339e5d222266ffbb
Andy Kreling <krelinga@gmail.com>
2026-01-10 18:20:40 +0000

    a simple Optional type.

--- go/lib/catalog/patch.go
+++ go/lib/catalog/patch.go
@@ -45,3 +45,47 @@ func (p *patchSetVal[T]) PtrPatch(ptr **T) {
 func Set[T any](val T) Patcher[T] {
 	return &patchSetVal[T]{val: val}
 }
+
+// Optional is a wrapper type that indicates whether a value is present or not.
+// It is always in one of two states:
+// - Nil: indicates that the value is not present.
+// - Non-nil: indicates that the value is present, and holds the value.
+// The zero value of Optional is the Nil state.
+type Optional[T any] struct {
+	val *T
+}
+
+// IsNil returns true if the Optional is in the Nil state.
+func (o Optional[T]) IsNil() bool {
+	return o.val == nil
+}
+
+// Get returns the value and a boolean indicating whether the value is present.
+// If the value is Nil, the zero value of T is returned.
+func (o Optional[T]) Get() (T, bool) {
+	if o.val == nil {
+		var zero T
+		return zero, false
+	}
+	return *o.val, true
+}
+
+// Must returns the value, panicking if the value is Nil.
+func (o Optional[T]) Must() T {
+	if o.val == nil {
+		panic("Optional: value is Nil")
+	}
+	return *o.val
+}
+
+// Ptr returns a pointer to the value, or nil if the value is Nil.
+// The returned pointer is a copy of the internal pointer, so modifying
+// the returned pointer does not affect the Optional (except for reference types).
+func (o Optional[T]) Ptr() *T {
+	if o.val == nil {
+		return nil
+	}
+	newVal := new(T)
+	*newVal = *o.val
+	return newVal
+}
